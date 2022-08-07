7e22c9901cda1fab5c9cbddefdb3cdfba198ebc9
hrahmonov2002 <107402912+hrahmonov2002@users.noreply.github.com>
2022-08-07 15:21:56 +0500

    Add script js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var elForm = document.querySelector(".site-form");
+var elTemp = elForm.querySelector(".site-temp");
+var elCheck = elForm.querySelector(".site-check");
+
+var elResult = document.querySelector(".result");
+
+const EMPT = "";
+const MIN_TEMP = 5;
+const MAX_TEMP = 40;
+
+elForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let privillage = elCheck.checked;
+
+   if (elTemp.value === EMPT) {
+     elResult.textContent = "Please, enter number";
+   } else if (elTemp.value < MIN_TEMP && privillage) {
+        elResult.textContent = "You can not run";
+    } else if (elTemp.value > MAX_TEMP || privillage) {
+        elResult.textContent = "You can not run";
+    } else {
+        elResult.textContent = "You can go running";
+    } 
+   
+    
+})
